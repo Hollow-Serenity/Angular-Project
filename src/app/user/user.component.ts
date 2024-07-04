bafa7527cca0b07d597fa98c066380c617899f56
Hollow-Serenity <dino.cosic1999@gmail.com>
2024-07-04 18:57:58 +0200

    implemented Signals for state management. mostly since this is an Angular 16+ project

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,11 +11,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  // Using Signal for state management.
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
   /**
    * This method is used to select a random user from the dummy users list.
@@ -28,6 +30,6 @@ export class UserComponent {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
     // Update the selectedUser property with the new random user
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
