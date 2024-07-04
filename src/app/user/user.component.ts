3bc224c43a86576966ad262275f8b9c2118dfc0c
Hollow-Serenity <dino.cosic1999@gmail.com>
2024-07-04 18:39:02 +0200

    managing user state and data.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,4 +12,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
   selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  /**
+   * This method is used to select a random user from the dummy users list.
+   * It updates the `selectedUser` property with the new random user.
+   *
+   * @returns {void}
+   */
+  onSelectUser() {
+    // Generate a random index within the range of the dummy users array
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+    // Update the selectedUser property with the new random user
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
