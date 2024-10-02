d9110dd9c8bd842aa7695ff5a8a9160edfd2d41d
Hollow-Serenity <dino.cosic1999@gmail.com>
2024-10-02 22:38:38 +0200

    minor source code formatting

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, output } from '@angular/core';
+import { Component, Input, Output, EventEmitter, output, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,20 +8,21 @@ import { Component, Input, Output, EventEmitter, output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-
+  @Input({required: true}) user!: {
+    id: string;
+    name: string;
+    avatar: string;
+  }
   @Output() select = new EventEmitter();
 
   // A different approach of getting the EventEmitter is using the following syntax:
   // select = output<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
